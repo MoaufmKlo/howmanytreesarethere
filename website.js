3ab9774bac4b2e58957d1987de5e9791eb39dbc3
MoaufmKlo <45636897+MoaufmKlo@users.noreply.github.com>
2020-08-22 11:01:33 +0200

    ✨Add years estimate
    Add an estimate amount of years until all trees will be gone

--- website.js
+++ website.js
@@ -4,4 +4,5 @@ setInterval(function() {
 
     // Set the trees text to the trees counted in 2015 subtracted by the number of seconds between now and 2015 multiplied by the amount of trees cut down every second
     document.getElementById("total").innerHTML = "There are<br><strong>" + Math.round(3204668000000 - (seconds * 475.64)).toLocaleString("de-CH") + "</strong><br>trees worldwide. 🌳";
+    document.getElementById("total2").innerHTML = `That means that in <br><strong style="font-size: 1em;">${(Math.round(3204668000000 - (seconds * 475.64))/450/60/60/24/365).toFixed(8)}</strong><br> years, there won't be any at all.`
 }, 20); // Repeat counter every 20ms (50x a second)
